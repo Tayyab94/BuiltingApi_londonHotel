46f439488193c3eb81e522a63934566ce4234324
tayyab94 <tayyab.bhatti30@gmail.com>
2019-11-22 06:23:16 -0800

    we have implemented Basic APIs and Versioning and Error handling Section

---
+++ London_Api_corePractice/Controllers/RouterController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace London_Api_corePractice.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class RouterController : ControllerBase
+    {
+
+        [HttpGet(Name = nameof(GetRout))]
+        [ProducesResponseType(200)]
+        public IActionResult GetRout()
+        {
+            var respose = new
+            {
+                href = Url.Link(nameof(GetRout), null),
+                rooms = Url.Link(nameof(RoomController.GetRooms), null)
+            };
+
+            return Ok(respose);
+        }
+    }
+}
---
+++ London_Api_corePractice/Filters/JsonExceptionFilter.cs
@@ -0,0 +1,43 @@
+﻿using London_Api_corePractice.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace London_Api_corePractice.Filters
+{
+    public class JsonExceptionFilter : IExceptionFilter
+    {
+        private readonly IHostingEnvironment environment;
+
+        public JsonExceptionFilter(IHostingEnvironment environment)
+        {
+            this.environment = environment;
+        }
+        public void OnException(ExceptionContext context)
+        {
+
+            var error = new ApiError();
+            if(environment.IsDevelopment())
+            {
+
+                error.Message = context.Exception.Message;
+                error.Details = context.Exception.StackTrace;
+            }
+            else
+            {
+
+                error.Message = "A server Error Occour...";
+                error.Details = context.Exception.Message;
+            }
+
+            context.Result = new ObjectResult(error)
+            {
+                StatusCode = 500
+            };
+        }
+    }
+}
