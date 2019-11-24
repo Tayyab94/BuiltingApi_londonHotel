c71af48fb11df872efc4a512c2823c6bc0c92148
tayyab94 <tayyab.bhatti30@gmail.com>
2019-11-24 07:52:43 -0800

    Reprenset Resources (Create Data Model Classes

--- London_Api_corePractice/HotelAPIDbContext.cs
+++ London_Api_corePractice/HotelAPIDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using London_Api_corePractice.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,9 @@ namespace London_Api_corePractice
 
         }
 
+        public DbSet<RoomEntity> Rooms
+        {
+            get; set;
+        }
     }
 }
