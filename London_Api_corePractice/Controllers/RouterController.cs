48d0dc5fb8ca68268aa965fc6996e32e01d46e36
tayyab94 <tayyab.bhatti30@gmail.com>
2019-11-25 09:40:28 -0800

    Represned Link (Create Link Classes) Insted of Url.Link MEthod

--- London_Api_corePractice/Controllers/RouterController.cs
+++ London_Api_corePractice/Controllers/RouterController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using London_Api_corePractice.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +19,13 @@ namespace London_Api_corePractice.Controllers
         {
             var respose = new
             {
-                href = Url.Link(nameof(GetRout), null),
-                rooms = Url.Link(nameof(RoomController.GetRooms), null),
-                info=Url.Link(nameof(InfoController.GetInfo),null)
+                //href = Url.Link(nameof(GetRout), null),
+                //rooms = Url.Link(nameof(RoomController.GetRooms), null),
+                //info=Url.Link(nameof(InfoController.GetInfo),null)
+                href = string.Empty,
+                rooms = Link.To(nameof(RoomController.GetRooms)),
+                info =Link.To(nameof(InfoController.GetInfo))
+
             };
 
             return Ok(respose);
---
+++ London_Api_corePractice/Models/Link.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace London_Api_corePractice.Models
+{
+    public class Link
+    {
+        public const string GetMethod = "Get";
+
+        public static Link To(string routeName, object routeValues = null) => new Link
+        {
+            RouteName = routeName,
+            RouteValue = routeValues,
+            Method = GetMethod,
+            Reltions = null
+        };
+
+        [JsonProperty(Order =-4)]
+        public string Href { get; set; }
+
+        [JsonProperty(Order =-3,
+            PropertyName ="rel"
+            ,NullValueHandling =NullValueHandling.Ignore)]
+        public string[] Reltions { get; set; }
+
+
+        [JsonProperty(Order =-2,
+            DefaultValueHandling =DefaultValueHandling.Ignore
+            ,NullValueHandling =NullValueHandling.Ignore)]
+        [DefaultValue(GetMethod)]
+        public string Method { get; set; }
+
+
+        //Store Route Name before being rewrtten by the Link RewritingFilter
+        [JsonIgnore]
+        public string RouteName { get; set; }
+
+        //Store Route Parameter before being rewrtten by the Link RewritingFilter
+        [JsonIgnore]
+        public object RouteValue { get; set; }
+    }
+}
